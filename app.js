296525b562eb4589e98382220a3bf2f81227bc1f
IgnacioBarlettaTripodi <ignacio@barlettatripodi.com>
2022-11-01 20:16:25 -0300

    subiendo archivos

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const app = express();
 
-let PORT = 3000
+//let PORT = 3000
+let PORT = process.send.PORT || 3000;
 
 const path = require('path');
 
-app.listen(PORT, console.log('Listen on port 3000', PORT));
+app.listen(PORT, console.log('Listen on port', PORT));
 
 app.use(express.static("public"));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname,"./views/home.html"))
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname,"./views/login.html"))
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname,"./views/register.html"))
 })
